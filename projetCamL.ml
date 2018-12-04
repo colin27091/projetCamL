0c5bbd8cceda16de9eecf0fbb3a87325c5fd4f83
MofidK <mkrim.imp@gmail.com>
2018-12-04 13:43:49 +0100

    qsf

--- projetCamL.ml
+++ projetCamL.ml
@@ -10,137 +10,50 @@
 (* let ligne3 = input_line ouvrir;; *)
 (* (* ligne3 : string = "wash" *) *)
 
-(* let nomFic = "C:/Users/Mofid Krim/Documents/Licence Informatique/L3/Caml/Projet Caml/dictionnaire_min.txt";; *)
-let nomFic = "C:/Users/c/Documents/L3Info/Projet Caml/dictionnaire_min.txt";;
 
+let nomFic = "C:/Users/Mofid Krim/Documents/Licence Informatique/L3/Caml/projetCamL/dictionnaire_min.txt";;
+(* let nomFic = "C:/Users/c/Documents/L3Info/Projet Caml/dictionnaire_min.txt";; *)
 
+(* let process_file process filename = *)
+    (* let ch = open_in filename in *)
+    (* try  *)
+		(* while true  *)
+			(* do process(input_line ch)  *)
+			(* done *)
+    (* with End_of_file -> close_in ch;; *)
+(* process_file = (string -> 'a) -> string -> unit = <fun> *)
 
+(* process_file print_endline nomFic;; *)
 
 
 
+(* let file_to_list = fun filename -> let ch = open_in filename in *)
+	(* try *)
+		(* while true *)
+		(* do *)
+			(* [input_line ch] @ [input_line ch] *)
+		(* done *)
+	(* with End_of_file -> close_in ch;; *)
 
 
+(* let rec process_file = fun filename -> let ch = open_in filename in *)
+	  (* -> input_line ch :: input_line ch *)
+    (* | End_of_file -> close_in ch;; *)
 
+(* file_to_list nomFic;; *)
 
+(* ouvrir : in_channel = <abstr> *)
 
+let ouvrir = open_in nomFic;;
 
+let ligne = input_line ouvrir;;
 
+let l = [];;
+[ligne]@l;;
 
+let ouvrir_fichier = fun 
+filename-> let ouverture = open_in filename in input_line ouverture;;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let file_to_list = fun filename -> let ch = open_in filename in
-	try
-		while true
-		do
-			input_line ch :: input_line ch
-		done
-	with End_of_file -> [];;
-
-
-let rec process_file = fun filename -> let ch = open_in filename in
-	  -> input_line ch :: input_line ch
-    | End_of_file -> close_in ch;;
-
-process_file nomFic;;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ouvrir_fichier nomFic;;
 
 
