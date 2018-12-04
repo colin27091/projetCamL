8911dff1e39ca54d3d2ea168836fa3c8601cb902
colin27091 <colinregourd@gmail.com>
2018-12-05 00:52:21 +0100

    file_to_list

--- projetCamL.ml
+++ projetCamL.ml
@@ -1,34 +1,30 @@
 
 let nomFic = "C:/Users/Mofid Krim/Documents/Licence Informatique/L3/Caml/projetCamL/dictionnaire_min.txt";;
-(* let nomFic = "C:/Users/c/Documents/L3Info/Projet Caml/dictionnaire_min.txt";; *)
+let nomFic = "C:/Users/c/Documents/L3Info/Projet Caml/dictionnaire_min.txt";; 
 
 
-
-let file_to_list = fun filename -> let ch = open_in filename in
+let rec canal_to_list = fun ch -> 
 	try
-		input_line ch
-	with End_of_file -> close_in and [];;
+		let input = input_line ch in
+		input :: canal_to_list ch
+	with End_of_file -> [];;
+
+let file_to_list = fun filename -> let ch = open_in filename in 
+	canal_to_list ch;;
+	
+	
+type arbre = Feuille of bool
+| Noeud of char*arbre*arbre ;;
 
 
-let rec process_file = fun filename -> let ch = open_in filename in
-	  -> input_line ch :: input_line ch
-    | End_of_file -> close_in ch;;
-
-process_file nomFic;;
 
 
 
-let ouvrir = open_in nomFic;;
 
-let ligne = input_line ouvrir;;
 
-let l = [];;
-[ligne]@l;;
 
-(* Affiche la premiere ligne du dico *)
-let fichier_to_list = fun 
-filename-> let ouverture = open_in filename in let ch = input_line ouverture in let l=[] in ch::l;;
 
-fichier_to_list nomFic;;
+
+
 
 
